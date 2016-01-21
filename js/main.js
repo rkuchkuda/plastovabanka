5e42efb88180a1ec5d87065f99695deebbf1af92
Roman <romko43@gmail.com>
2016-01-21 13:37:14 +0000

    add start files whith bootstrap grid

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    console.log('ready');
+});
+
+$(window).load(function() {
+	console.log('load');
+});
+
+$(window).resize(function() {
+    console.log('resize');
+});
